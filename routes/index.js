74b6b035c33e5a74cfe09a3496e5dc0de0d46977
chenjiefly <chenjiefly@163.com>
2015-06-28 21:16:04 +0800

    complete microblog as the first version

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user.js');
+var Post = require('../models/post.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', {
-        title: '首页',
-        user: req.session.user,
-        success: req.flash('success').toString(),
-        error: req.flash('error').toString()
+    Post.get(null, function(err, posts) {
+        if (err) {
+            posts = [];
+        }
+        res.render('index', {
+            title: '首页',
+            posts: posts,
+            user: req.session.user,
+            success: req.flash('success').toString(),
+            error: req.flash('error').toString()
+        });
     });
 });
 
